8357820f8dab736ad2d0eefcb3e3cdf40636038e
TheoQM <theo@quantum-machines.co>
2025-04-30 18:56:56 +0200

    Set get_qubits as a method of the QubitsExperimentNodeParameters

--- qualibration_libs/parameters/experiment.py
+++ qualibration_libs/parameters/experiment.py
@@ -19,40 +19,39 @@ class QubitsExperimentNodeParameters(RunnableParameters):
     """The qubit reset method to use. Must be implemented as a method of Quam.qubit. Can be "thermal", "active", or
     "active_gef". Default is "thermal"."""
 
+    def get_qubits(self, node: QualibrationNode) -> BatchableList[AnyTransmon]:
+        """Get the batchable list of active qubits according to the multiplexed parameter.
+        For instance [[machine.qubits["q1"]], [machine.qubits["q2"]], [machine.qubits["q3"]], [machine.qubits["q4"]]] for multiplexed==False
+        and [machine.qubits["q1"], machine.qubits["q2"], machine.qubits["q3"], machine.qubits["q4"]] for multiplexed==True.
+        """
+        qubits = self._get_qubits(node.machine)
 
-def get_qubits(node: QualibrationNode) -> BatchableList[AnyTransmon]:
-    # todo: make a method once https://github.com/qua-platform/qualibrate-core/pull/89 is merged
-    qubits = _get_qubits(node.machine, node.parameters)
+        if isinstance(node.parameters, QubitsExperimentNodeParameters):
+            multiplexed = node.parameters.multiplexed
+        else:
+            multiplexed = False
 
-    if isinstance(node.parameters, QubitsExperimentNodeParameters):
-        multiplexed = node.parameters.multiplexed
-    else:
-        multiplexed = False
+        qubits_batchable_list = self._make_batchable_list_from_multiplexed(
+            qubits, multiplexed
+        )
 
-    qubits_batchable_list = _make_batchable_list_from_multiplexed(qubits, multiplexed)
+        return qubits_batchable_list
 
-    return qubits_batchable_list
+    def _get_qubits(self, machine: AnyQuam) -> List[AnyTransmon]:
+        if self.qubits is None or self.qubits == "":
+            qubits = machine.active_qubits
+        else:
+            qubits = [machine.qubits[q] for q in self.qubits]
 
-def _get_qubits(
-    machine: AnyQuam, node_parameters: QubitsExperimentNodeParameters
-) -> List[AnyTransmon]:
-    # todo: make a method once https://github.com/qua-platform/qualibrate-core/pull/89 is merged
-    if node_parameters.qubits is None or node_parameters.qubits == "":
-        qubits = machine.active_qubits
-    else:
-        qubits = [machine.qubits[q] for q in node_parameters.qubits]
+        return qubits
 
-    return qubits
+    @staticmethod
+    def _make_batchable_list_from_multiplexed(
+        items: List, multiplexed: bool
+    ) -> BatchableList:
+        if multiplexed:
+            batched_groups = [[i for i in range(len(items))]]
+        else:
+            batched_groups = [[i] for i in range(len(items))]
 
-def _make_batchable_list_from_multiplexed(
-    items: List, multiplexed: bool
-) -> BatchableList:
-    if multiplexed:
-        batched_groups = [[i for i in range(len(items))]]
-    else:
-        batched_groups = [[i] for i in range(len(items))]
-
-    return BatchableList(items, batched_groups)
-
-
-
+        return BatchableList(items, batched_groups)
