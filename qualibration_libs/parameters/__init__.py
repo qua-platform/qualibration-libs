0c2a73c050515a769db003c859a5ec28c30df7aa
TheoQM <theo@quantum-machines.co>
2025-04-30 15:16:01 +0200

    add get_qubits to the init

--- qualibration_libs/parameters/__init__.py
+++ qualibration_libs/parameters/__init__.py
@@ -1,10 +1,11 @@
 from .common import CommonNodeParameters
-from .experiment import QubitsExperimentNodeParameters
+from .experiment import QubitsExperimentNodeParameters, get_qubits
 from .sweep import IdleTimeNodeParameters, get_idle_times_in_clock_cycles
 
 __all__ = [
     "CommonNodeParameters",
     "QubitsExperimentNodeParameters",
+    "get_qubits",
     "IdleTimeNodeParameters",
     "get_idle_times_in_clock_cycles",
 ]
