9dc6ac38a49d828c5d95db641e79ab5f35a349bb
HiroQM <hiroyuki.inoue@quantum-machines.co>
2025-06-24 20:10:00 +0900

    enable batch group& multiplexed iteration

--- qualibration_libs/parameters/experiment.py
+++ qualibration_libs/parameters/experiment.py
@@ -1,4 +1,4 @@
-from typing import List, Literal, Optional
+from typing import List, Literal, Optional, Tuple
 
 from qualibrate import QualibrationNode
 from qualibrate.parameters import RunnableParameters
@@ -31,7 +31,7 @@ class TwoQubitExperimentNodeParameters(BaseExperimentNodeParameters):
 
 
 def get_qubits(node: QualibrationNode) -> BatchableList[AnyTransmon]:
-    qubits = _get_qubits(node.machine, node.parameters)
+    qubits, batch_groups = _get_qubits(node.machine, node.parameters)
 
     if isinstance(node.parameters, QubitsExperimentNodeParameters):
         multiplexed = node.parameters.multiplexed
@@ -43,41 +43,78 @@ def get_qubits(node: QualibrationNode) -> BatchableList[AnyTransmon]:
     return qubits_batchable_list
 
 
-def _get_qubits(machine: AnyQuam, node_parameters: QubitsExperimentNodeParameters) -> List[AnyTransmon]:
+def _get_qubits(
+    machine: AnyQuam, node_parameters: QubitsExperimentNodeParameters
+) -> Tuple[List[AnyTransmon], Optional[List[List[int]]]]:
     if node_parameters.qubits is None or node_parameters.qubits == "":
         qubits = machine.active_qubits
+        batch_groups = [[i] for i in range(len(qubits))]
     else:
-        qubits = [machine.qubits[q] for q in node_parameters.qubits]
+        flat_index = 0
+        qubits: List[AnyTransmon] = []
+        batch_groups: List[List[int]] = []
+        for group in node_parameters.qubits:
+            if isinstance(group, str):
+                group = [group]
+            batch = []
+            for q in group:
+                qubits.append(machine.qubits[q])
+                batch.append(flat_index)
+                flat_index += 1
+            batch_groups.append(batch)
 
-    return qubits
+    return qubits, batch_groups
 
 
 def get_qubit_pairs(node: QualibrationNode) -> BatchableList[AnyTransmonPair]:
-    qubit_pairs = _get_qubit_pairs(node.machine, node.parameters)
+    qubit_pairs, batch_groups = _get_qubit_pairs(node.machine, node.parameters)
 
     if isinstance(node.parameters, TwoQubitExperimentNodeParameters):
         multiplexed = node.parameters.multiplexed
     else:
         multiplexed = False
 
-    qubit_pairs_batchable_list = _make_batchable_list_from_multiplexed(qubit_pairs, multiplexed)
+    qubit_pairs_batchable_list = _make_batchable_list_from_multiplexed(
+        qubit_pairs, multiplexed
+    )
 
     return qubit_pairs_batchable_list
 
 
-def _get_qubit_pairs(machine: AnyQuam, node_parameters: TwoQubitExperimentNodeParameters) -> List[AnyTransmonPair]:
+def _get_qubit_pairs(
+    machine: AnyQuam, node_parameters: TwoQubitExperimentNodeParameters
+) -> Tuple[List[AnyTransmonPair], Optional[List[List[int]]]]:
     if node_parameters.qubit_pairs is None or node_parameters.qubit_pairs == "":
         qubit_pairs = machine.active_qubit_pairs
+        batch_groups = [[i] for i in range(len(qubit_pairs))]
     else:
-        qubit_pairs = [machine.qubit_pairs[q] for q in node_parameters.qubit_pairs]
+        flat_index = 0
+        qubit_pairs: List[AnyTransmonPair] = []
+        batch_groups: List[List[int]] = []
+        for group in node_parameters.qubit_pairs:
+            if isinstance(group, str):
+                group = [group]
+            batch = []
+            for qp in group:
+                qubit_pairs.append(machine.qubit_pairs[qp])
+                batch.append(flat_index)
+                flat_index += 1
+            batch_groups.append(batch)
 
-    return qubit_pairs
+    return qubit_pairs, batch_groups
 
 
-def _make_batchable_list_from_multiplexed(items: List, multiplexed: bool) -> BatchableList:
+def _make_batchable_list_from_multiplexed(
+    items: List,
+    multiplexed: bool,
+    batch_groups: Optional[List[List[int]]] = None,
+) -> BatchableList:
+    if batch_groups is not None:
+        return BatchableList(items, batch_groups)
+
     if multiplexed:
-        batched_groups = [[i for i in range(len(items))]]
+        batch_groups = [[i for i in range(len(items))]]
     else:
-        batched_groups = [[i] for i in range(len(items))]
+        batch_groups = [[i] for i in range(len(items))]
 
-    return BatchableList(items, batched_groups)
+    return BatchableList(items, batch_groups)
