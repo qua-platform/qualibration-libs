d8cdec55de6ba06c764afb62d359948bf1928c16
HiroQM <hiroyuki.inoue@quantum-machines.co>
2025-07-25 18:23:41 +0900

    added iqcc cloud data processor

--- qualibration_libs/data/__init__.py
+++ qualibration_libs/data/__init__.py
@@ -1,7 +1,9 @@
 from .fetcher import XarrayDataFetcher
 from .processing import *
+from .cloud_processing import CloudDataProcessor
 
 __all__ = [
     *fetcher.__all__,
     *processing.__all__,
+    *cloud_processing.__all__,
 ]
